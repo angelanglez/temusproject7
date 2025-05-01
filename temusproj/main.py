7a91a77e857a7bc435c93461b900f9c53d7acb76
Angelina Wu <sakuratreepetals@gmail.com>
2025-05-01 22:39:46 +0800

    added input to graph.ipynb for personalised topics

---
+++ temusproj/main.py
@@ -0,0 +1,14 @@
+#write thinsg here, to move graph.ipynb 
+def main( ):
+    #code to run here 
+    query_no = 
+    while True:
+        #look for user input
+        user_input = input()
+        
+        if user_input != "":
+            query_no += 1
+
+        
+if __name__ == "__main__":
+    main( )
